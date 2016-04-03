0795cf6115806f1e0d700b4bc47e49d11f377d19
Tomer David <younrg@gmail.com>
2016-04-03 16:11:57 +0200

    finished extension 1 lvl

--- javascript/views/commentView.js
+++ javascript/views/commentView.js
@@ -7,12 +7,15 @@ var CommentView = Backbone.View.extend({
 
   removeComment: function () {
     // TODO: Destroy the comment model
+    this.collection.remove(this.model);
     // TODO: Remove the comment view
+    this.remove();
   },
 
   render: function () {
     var template = Handlebars.compile(this.template.html());
 
     this.$el.append(template(this.model.toJSON()));
+    return this;
   }
 });
